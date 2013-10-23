c6c659899f6454917affc2efa8aa476fbfb5643d
Karl Broman <kbroman@gmail.com>
2013-10-22 19:17:30 -0500

    Prepare widescreen, just in case

--- createVersionWithNotes.rb
+++ createVersionWithNotes.rb
@@ -9,6 +9,6 @@ ofile = File.open(ofilename, "w")
 
 ifile.readlines.each do |z|
   z = z.sub("hide", "show") if /\\setbeameroption{hide notes}/ =~ z
-  z = z.sub("[12pt,t]", "[12pt,t,handout]") if /\\documentclass\[12pt,t\]{beamer}/ =~ z
+  z = z.sub("[aspectratio=1610,12pt,t]", "[12pt,t,handout]") if /\\documentclass\[aspectratio=1610,12pt,t\]{beamer}/ =~ z
   ofile.write(z)
 end
